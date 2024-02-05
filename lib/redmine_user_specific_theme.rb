a90827dec3782e3f3e8e469a07f4436c26e2d834
林劭涵 <shaohanlin@mirle.com.tw>
2024-02-05 13:52:51 +0800

    Redmine 5.0 support

--- lib/redmine_user_specific_theme.rb
+++ lib/redmine_user_specific_theme.rb
@@ -5,14 +5,9 @@ require File.dirname(__FILE__) + '/redmine_user_specific_theme/hooks/view_hooks'
 require File.dirname(__FILE__) + '/redmine_user_specific_theme/patches/user_preference_patch'
 require File.dirname(__FILE__) + '/redmine_user_specific_theme/patches/application_helper_patch'
 
-ActiveSupport::Reloader.to_prepare do
-
-  unless UserPreference.included_modules.include?(RedmineUserSpecificTheme::Patches::UserPreferencePatch)
-    UserPreference.send :prepend, RedmineUserSpecificTheme::Patches::UserPreferencePatch
-  end
-
-  unless ApplicationHelper.included_modules.include?(RedmineUserSpecificTheme::Patches::ApplicationHelperPatch)
-    ApplicationHelper.send :include, RedmineUserSpecificTheme::Patches::ApplicationHelperPatch
-  end
-
+unless UserPreference.included_modules.include?(RedmineUserSpecificTheme::Patches::UserPreferencePatch)
+  UserPreference.send :prepend, RedmineUserSpecificTheme::Patches::UserPreferencePatch
+end
+unless ApplicationHelper.included_modules.include?(RedmineUserSpecificTheme::Patches::ApplicationHelperPatch)
+  ApplicationHelper.send :include, RedmineUserSpecificTheme::Patches::ApplicationHelperPatch
 end
